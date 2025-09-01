7d06998d168c16e42ee553f98007fd0769761c42
maciejsobieniak <maciej@maciejsobieniak.pl>
2025-09-01 18:53:14 +0200

    7.6 Added kodilla-stream-portfolio module

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -0,0 +1,171 @@
+package com.kodilla.stream.portfolio;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTestSuite {
+
+    @Test
+    void testAddTaskList() {
+        //Given
+        Board project = prepareTestData();
+        //When
+
+        //Then
+        assertEquals(3, project.getTaskLists().size());
+    }
+
+    @Test
+    @DisplayName("Test to find out all tasks are assigned to a specific user")
+    void testAddTaskListFindUsersTasks() {
+        //Given
+        Board project = prepareTestData();
+        //When
+        User user = new User("developer1", "John Smith");
+        List<Task> tasks = project.getTaskLists().stream()
+                .flatMap(taskList -> taskList.getTasks().stream())
+                .filter(task -> task.getAssignedUser().equals(user))
+                .collect(Collectors.toList());
+        //Then
+        assertEquals(2, tasks.size());
+        assertEquals(user, tasks.get(0).getAssignedUser());
+        assertEquals(user, tasks.get(1).getAssignedUser());
+    }
+
+    @Test
+    @DisplayName("Test to find out all tasks that are not yet finished")
+    void testAddTaskListFindOutdatedTasks() {
+        //Given
+        Board project = prepareTestData();
+        //When
+        List<Task> undoneTasks = project.getTaskLists().stream()
+                .filter(taskList -> !taskList.getName().equals("Done"))
+                .flatMap(taskList -> taskList.getTasks().stream())
+                .filter(task -> task.getDeadline().isBefore(LocalDate.now()))
+                .collect(Collectors.toList());
+        //Then
+        assertEquals(1, undoneTasks.size());
+        assertEquals("HQLs for analysis", undoneTasks.get(0).getTitle());
+    }
+
+    @Test
+    @DisplayName("Test to find out all tasks that are in progress for more than 10 days")
+    void testAddTaskListFindLongTasks() {
+        //Given
+        Board project = prepareTestData();
+        //When
+        List<Task> longTasks = project.getTaskLists().stream()
+                .filter(taskList -> taskList.getName().equals("In progress"))
+                .flatMap(taskList -> taskList.getTasks().stream())
+                .filter(task -> task.getCreated().isBefore(LocalDate.now().minusDays(10)))
+                .collect(Collectors.toList());
+        //Then
+        assertEquals(1, longTasks.size());
+        assertEquals("HQLs for analysis", longTasks.getFirst().getTitle());
+    }
+
+    /**
+     * another version of the last test using count() method
+     */
+
+    /**
+    @Disabled
+    @Test
+    void testAddTaskListFindLongTasks() {
+        //Given
+        Board project = prepareTestData();
+
+        //When
+        List<TaskList> inProgressTasks = new ArrayList<>();               // [1]
+        inProgressTasks.add(new TaskList("In progress"));                 // [2]
+        long longTasks = project.getTaskLists().stream()                  // [3]
+                .filter(inProgressTasks::contains)                             // [4]
+                .flatMap(tl -> tl.getTasks().stream())                         // [5]
+                .map(Task::getCreated)                                         // [6]
+                .filter(d -> d.compareTo(LocalDate.now().minusDays(10)) <= 0)  // [7]
+                .count();                                                      // [8]
+
+        //Then
+        assertEquals(2, longTasks);                                       // [9]
+    }*/
+
+
+    /**
+     * Method to prepare test data
+     *
+     * @return obiect of Board class with test data
+     */
+
+    private Board prepareTestData() {
+        //users
+        User user1 = new User("developer1", "John Smith");
+        User user2 = new User("projectmanager1", "Nina White");
+        User user3 = new User("developer2", "Emilia Stephanson");
+        User user4 = new User("developer3", "Konrad Bridge");
+
+        //tasks
+        Task task1 = new Task("Microservice for taking temperature",
+                "Write and test the microservice taking\n" +
+                        "the temperaure from external service",
+                user1,
+                user2,
+                LocalDate.now().minusDays(20),
+                LocalDate.now().plusDays(30));
+        Task task2 = new Task("HQLs for analysis",
+                "Prepare some HQL queries for analysis",
+                user1,
+                user2,
+                LocalDate.now().minusDays(20),
+                LocalDate.now().minusDays(5));
+        Task task3 = new Task("Temperatures entity",
+                "Prepare entity for temperatures",
+                user3,
+                user2,
+                LocalDate.now().minusDays(20),
+                LocalDate.now().plusDays(15));
+        Task task4 = new Task("Own logger",
+                "Refactor company logger to meet our needs",
+                user3,
+                user2,
+                LocalDate.now().minusDays(10),
+                LocalDate.now().plusDays(25));
+        Task task5 = new Task("Optimize searching",
+                "Archive data searching has to be optimized",
+                user4,
+                user2,
+                LocalDate.now(),
+                LocalDate.now().plusDays(5));
+        Task task6 = new Task("Use Streams",
+                "use Streams rather than for-loops in predictions",
+                user4,
+                user2,
+                LocalDate.now().minusDays(15),
+                LocalDate.now().minusDays(2));
+
+        //taskLists
+        TaskList taskListToDo = new TaskList("To do");
+        taskListToDo.addTask(task1);
+        taskListToDo.addTask(task3);
+        TaskList taskListInProgress = new TaskList("In progress");
+        taskListInProgress.addTask(task5);
+        taskListInProgress.addTask(task4);
+        taskListInProgress.addTask(task2);
+        TaskList taskListDone = new TaskList("Done");
+        taskListDone.addTask(task6);
+
+        //board
+        Board project = new Board("Project Weather Prediction");
+        project.addTaskList(taskListToDo);
+        project.addTaskList(taskListInProgress);
+        project.addTaskList(taskListDone);
+        return project;
+    }
+}
